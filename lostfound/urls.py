4c268f9c5c887d5983e81b8d973f892e7169e195
Augustas Stancikas <augustas.stancikas@gmail.com>
2025-11-26 00:46:45 +0100

    Enable custom 404 and 500 handlers

--- lostfound/urls.py
+++ lostfound/urls.py
@@ -11,3 +11,14 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+from django.conf.urls import handler404, handler500
+
+handler404 = 'core.views.custom_404'
+handler500 = 'core.views.custom_500'
+
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('core.urls')),   # or your home view
+]

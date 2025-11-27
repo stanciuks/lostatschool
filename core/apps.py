70276df9ec22f7c821de16b3b8ea7cdd3aeff62f
Augustas Stancikas <augustas.stancikas@gmail.com>
2025-11-27 10:28:15 +0100

    Updated templates and styling
    git push

--- core/apps.py
+++ core/apps.py
@@ -3,9 +3,4 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
-
-    def ready(self):
-        # Temporarily disabled automatic category creation (causes crash before migrations)
-        pass
-
-
+      
